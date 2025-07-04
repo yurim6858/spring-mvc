62b5127ad3611a580b87ecd24b7a144ecee7bb6d
Yurim <yurim6858@g.hongik.ac.kr>
2025-07-04 15:00:23 +0900

    feat: Get/Post 요청 테스트

--- src/main/java/com/metaverse/springmvc/controller/HelloController.java
+++ src/main/java/com/metaverse/springmvc/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.metaverse.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -11,5 +12,17 @@ public class HelloController {
     @ResponseBody
     public String hello(){
         return "Hello World!";
+    }
+
+    @GetMapping("api/get")
+    @ResponseBody
+    public String get(){
+        return "Get Method 요청";
+    }
+
+    @PostMapping("api/post")
+    @ResponseBody
+    public String post(){
+        return "Post Method 요청";
     }
 }
