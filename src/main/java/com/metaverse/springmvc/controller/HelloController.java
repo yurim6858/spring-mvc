f9b027738ce5bcdebb78bb2f115fde9b622764fe
Yurim <yurim6858@g.hongik.ac.kr>
2025-07-04 14:42:15 +0900

    feat: 컨트롤러 생성

---
+++ src/main/java/com/metaverse/springmvc/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.metaverse.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("api/hello")
+    @ResponseBody
+    public String hello(){
+        return "Hello World!";
+    }
+}
