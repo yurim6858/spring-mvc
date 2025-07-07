d9acbf38a8e7362de552740b047508b692eb5390
Yurim <yurim6858@g.hongik.ac.kr>
2025-07-07 10:54:47 +0900

    feat: 뷰템플릿엔진 - 템플릿엔진을 통한 뷰 변환

---
+++ src/main/java/com/metaverse/springmvc/controller/HelloViewController.java
@@ -0,0 +1,22 @@
+package com.metaverse.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloViewController {
+    @GetMapping("/static-hello")
+    public String hello(){
+        return "hello.html";
+    }
+
+    @GetMapping("/html/redirect") // 이 주소로는 못 들어감
+    public String htmlRedirect(){
+        return "redirect:/hello.html";
+    }
+
+    @GetMapping("/html/templetes") //이 주소로 들어가고 싶으면 thymeleaf라는 라이브러리가 있어야함.
+    public String htmlTempletes(){ // 템플릿 폴더에 넣어야함. 폴더마다 기능이 다름
+        return "hello-templete";
+    }
+}
