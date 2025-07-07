3d4cc7eeff43d056fa56ca08a404026176f39897
Yurim <yurim6858@g.hongik.ac.kr>
2025-07-07 15:12:21 +0900

    feat: json java 객체 반환

--- src/main/java/com/metaverse/springmvc/controller/DataResponseController.java
+++ src/main/java/com/metaverse/springmvc/controller/DataResponseController.java
@@ -1,5 +1,6 @@
 package com.metaverse.springmvc.controller;
 
+import com.metaverse.springmvc.domain.Star;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,5 +11,11 @@ public class DataResponseController {
     @ResponseBody
     public String helloStringJson(){
         return "{\"name\" : \"Meta\" , \"age\" : 95}";// " 대신 \"로 대체
+    }
+
+    @GetMapping("response/json/instance")
+    @ResponseBody
+    public Star helloInstanceJson(){
+        return new Star("star",5); // = Star star = new Star();
     }
 }
---
+++ src/main/java/com/metaverse/springmvc/domain/Star.java
@@ -0,0 +1,14 @@
+package com.metaverse.springmvc.domain;
+
+import lombok.Getter;
+
+@Getter
+public class Star {
+    String name;
+    int age;
+
+    public Star(String name,int age){
+        this.name=name;
+        this.age=age;
+    }
+}
