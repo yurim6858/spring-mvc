d6cfa83666a5b3968bf7ea9e10454887d6b7260b
Yurim <yurim6858@g.hongik.ac.kr>
2025-07-07 15:33:42 +0900

    test: Object to JSON 파싱, 잭슨라이브러리가 수행

---
+++ src/test/java/com/metaverse/springmvc/JsonTest.java
@@ -0,0 +1,19 @@
+package com.metaverse.springmvc;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.metaverse.springmvc.domain.Star;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class JsonTest {
+    @Test
+    @DisplayName("Object to JSON : Get method 필요")
+    void test1() throws JsonProcessingException {
+        Star star = new Star("Meta",55);
+
+        ObjectMapper objectMapper = new ObjectMapper(); //Jackson 라이브러리의 ObjectMapper가 파싱 기능을 수행한다.
+        String jsonString = objectMapper.writeValueAsString(star);
+        System.out.println("json Result :" + jsonString);
+    }
+}
