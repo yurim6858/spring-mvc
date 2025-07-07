c0cf3d4cdc518d013c8e750aed932dd1f8112e4f
Yurim <yurim6858@g.hongik.ac.kr>
2025-07-07 12:38:08 +0900

    feat: 뷰템플릿엔진 -  Model 객체를 통한 동적데이터바인딩

--- src/main/java/com/metaverse/springmvc/controller/HelloViewController.java
+++ src/main/java/com/metaverse/springmvc/controller/HelloViewController.java
@@ -1,10 +1,13 @@
 package com.metaverse.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloViewController {
+    private static long visitCount = 0; //변수들은 계속 여기 생성
+
     @GetMapping("/static-hello")
     public String hello(){
         return "hello.html";
@@ -19,6 +22,13 @@ public class HelloViewController {
     public String htmlTempletes(){ // 템플릿 폴더에 넣어야함. 폴더마다 기능이 다름
         // prefix classpath : resources까지의 경로 + templates/ 까지 가지고 있음
         // suffix .html
-        return "hello-templete";
+        return "hello-templete";//String 반환 말고 해당 문자열을 가진 파일이 return
+    }
+
+    @GetMapping("html/dynamic")
+    public String htmlDynamic(Model model){ // html로 넘겨 동적으로 만들기 위해 모델 모델을 매게 변수로 넣어줌
+        visitCount++;
+        model.addAttribute("visits", visitCount); // html에서 쓰일 변수명 : 데이터 binding
+        return "hello-dynamic";
     }
 }
