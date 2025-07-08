41024e52c0de7c1b5832d900210870fd5cea751f
Yurim <yurim6858@g.hongik.ac.kr>
2025-07-08 12:23:54 +0900

    feat: QueryString 방식 요청 받기

--- src/main/java/com/metaverse/springmvc/controller/DataRequestController.java
+++ src/main/java/com/metaverse/springmvc/controller/DataRequestController.java
@@ -1,10 +1,7 @@
 package com.metaverse.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/hello/request")
@@ -19,5 +16,12 @@ public class DataRequestController {
     @ResponseBody
     public String helloRequestPath(@PathVariable String name, @PathVariable Integer age) {
         return String.format("Hello, @PathVariable로 받은 값들, <br> name = %s, age = %d", name, age);
+    }
+
+    //QueryString 방식 요청 받는 방법 (@RequestParem)
+    @GetMapping("/form/param")
+    @ResponseBody
+    public String helloRequestParam(@RequestParam String name, @RequestParam Integer age) {
+        return String.format("Hello, @PathVariable로 받은 값들,<br> name = %s, age = %d", name, age);
     }
 }
