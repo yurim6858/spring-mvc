9593b1d82bb33db905898524acd8bc7f90d629a5
Yurim <yurim6858@g.hongik.ac.kr>
2025-07-08 14:18:57 +0900

    feat: JSON(@RequestBody) to Object Post 요청 확인

--- src/main/java/com/metaverse/springmvc/controller/DataRequestController.java
+++ src/main/java/com/metaverse/springmvc/controller/DataRequestController.java
@@ -1,5 +1,6 @@
 package com.metaverse.springmvc.controller;
 
+import com.metaverse.springmvc.domain.Star;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,13 +23,31 @@ public class DataRequestController {
     @GetMapping("/form/param")
     @ResponseBody
     public String helloRequestParam(@RequestParam String name, @RequestParam Integer age) {
-        return String.format("Hello, @PathVariable로 받은 값들,<br> name = %s, age = %d", name, age);
+        return String.format("Hello, @RequestParam로 받은 값들,<br> name = %s, age = %d", name, age);
     }
 
     //QueryString 방식 Post 요청 받는 방법
     @PostMapping("/form/param")
     @ResponseBody
     public String helloRequestParamPost(@RequestParam String name, @RequestParam Integer age) {
-        return String.format("Hello, @PathVariavle로 받은 값들,<br> name = %s, age = %d", name, age);
+        return String.format("Hello, @RequestParam로 받은 값들,<br> name = %s, age = %d", name, age);
+    }
+
+    @PostMapping("/form/model")
+    @ResponseBody
+    public String helloRequestModel(@ModelAttribute Star star) {
+        return String.format("Hello,@ModelAttribute로 받은 객체 Post요청, <br> name = %s, age = %d", star.getName(), star.getAge());
+    }
+
+    @GetMapping("/form/param/model")
+    @ResponseBody
+    public String helloRequestModel2(@ModelAttribute Star star) {
+        return String.format("Hello,@ModelAttribute로 받은 객체 Get요청, <br> name = %s, age = %d", star.getName(), star.getAge());
+    }
+
+    @PostMapping("/form/json")
+    @ResponseBody
+    public String helloPostRequestJson(@RequestBody Star star) {
+        return String.format("Hello,@RequestBody로 받은 JSON -> 객체로 받기, <br> name = %s, age = %d", star.getName(), star.getAge());
     }
 }
--- src/main/java/com/metaverse/springmvc/domain/Star.java
+++ src/main/java/com/metaverse/springmvc/domain/Star.java
@@ -3,8 +3,10 @@ package com.metaverse.springmvc.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @AllArgsConstructor // 이 두개로 메서드를 안써도 축약됨
 @NoArgsConstructor
 public class Star {
